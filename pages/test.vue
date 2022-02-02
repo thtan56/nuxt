503bfcbb5ceaa9f623cee22fbe503c42f34492de
tan thian huat <t.h.tan@dunelm.org.uk>
2022-02-02 12:58:58 +0800

    Initial commit

---
+++ pages/test.vue
@@ -0,0 +1,42 @@
+<template>
+  <PageWrapper>
+    <PageHeader>
+      <PageTitle text="TESTING" />
+    </PageHeader>
+    <PageBody>
+      <PageSection>
+        <PageSectionTitle text="Counter" />
+        <div class="flex flex-col items-center justify-items-center space-y-2 lg:space-y-0 lg:flex-row lg:space-x-2">
+          <div>Counter : {{ counter.count }}</div>
+          <Button class="w-full lg:w-auto" type="secondary" size="sm" text="Increment" @click.prevent="counter.increment" />
+          <Button class="w-full lg:w-auto" type="secondary" size="sm" text="Increment 2x" @click.prevent="counter.increment2x" />
+          <Button class="w-full lg:w-auto" type="secondary" size="sm" text="Decrement" @click.prevent="counter.decrement" />
+          <Button class="w-full lg:w-auto" type="secondary" size="sm" text="Reset" @click.prevent="counter.reset" />
+        </div>
+      </PageSection>
+      <PageSection>
+        <PageSectionTitle text="Identity" />
+        <div>Full Name : {{ identity.fullName }}</div>
+        <div class="">
+          <div class="flex flex-col items-center space-y-2 lg:space-y-0 lg:flex-row lg:space-x-2">
+            <FormTextInput size="sm" v-model:value="identity.firstName" />
+            <FormTextInput size="sm" v-model:value="identity.lastName" />
+            <Button class="w-full lg:w-auto" text="Reset" type="secondary" size="sm" @click.prevent="identity.reset" />
+          </div>
+        </div>
+      </PageSection>
+    </PageBody>
+  </PageWrapper>
+</template>
+
+<script lang="ts" setup>
+import { useCounter } from "~/store/counter"
+import { useIdentity } from "~/store/identity";
+
+const counter = useCounter()
+const identity = useIdentity()
+
+definePageMeta({
+  layout: 'page',
+})
+</script>
